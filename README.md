d45b0b496a53e181ae4762705b188edd026c0d30
Matt Gauger <matt.gauger@gmail.com>
2022-07-03 11:16:29 -0500

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nand2tetris-test-generator
 Generates a test script (.tst) for the nand2tetris Hardware Simulator from a truth table
+
+It does not yet handle any other chips than the ALU, although I'd like it to learn to read the inputs and outputs and generate the correct `output-list` values. This also doesn't have any tests beyond me manually diffing the output against the provided `ALU` test scripts.
+
+```
+Usage: cmp-to-test-script.rb [options]
+    -f--file FILE                    Compare file to turn into a test script
+    -v, --[no-]verbose               Run verbosely
+    -h, --help                       Prints this help
+```
+
+Example:
+
+```bash
+./$PATH_TO_SCRIPT/cmp-to-test-script.rb -v -f ALU-nostat.cmp > ALU-my-notstat.tst
+```
+
+## [License: MIT](/blob/main/LICENSE)
---
+++ cmp-to-test-script.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+#
+require 'csv'
+require 'optparse'
+
+class CmpToTestScript
+  def initialize(data:, output_file:, input_file:, verbose:)
+    @data = data
+
+    # For now, we just standardize on the ALU inputs and outputs.
+    # TODO: Make dynamic in the future!
+    @input_pins = ["x", "y", "zx", "nx", "zy", "ny", "f", "no"]
+    @output_pins = ["out"] # TODO: also zr and ng
+
+    @input_filename = input_file
+    @output_filename = output_file
+    @verbose = verbose
+  end
+
+  def run
+    output = ""
+    # Preamble for ALU
+    preamble = <<~END
+      load ALU.hdl,
+      output-file #{@input_filename.sub(/\.cmp\Z/, '.out')},
+      compare-to #{@input_filename},
+      output-list x%B1.16.1 y%B1.16.1 zx%B1.1.1 nx%B1.1.1 zy%B1.1.1
+        ny%B1.1.1 f%B1.1.1 no%B1.1.1 out%B1.16.1;
+
+    END
+
+    p preamble
+    output << preamble
+
+    @data.each do |row|
+      test = ""
+      @input_pins.each do |input|
+        val = row[input]
+        val = "%B#{val}" if row[input].length > 1
+
+        test << "set #{input} #{val},\n"
+      end
+
+      test << "eval,\n"
+      test << "output;\n\n"
+
+      p test
+      output << test
+    end
+  end
+
+  def p(str)
+    puts str if @verbose
+  end
+end
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: cmp-to-test-script.rb [options]"
+
+  opts.on('-f' "--file FILE", "Compare file to turn into a test script") do |file|
+    options[:file] = file
+  end
+
+  opts.on('-o' "--output FILE", "Output test script filename. Overwrites existing.") do |file|
+    options[:output_file] = file
+  end
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-h", "--help", "Prints this help") do |v|
+    options[:help] = true
+    puts opts
+  end
+end.parse!
+
+raise "Must provide file to this script!" unless options[:file] && !options[:help]
+
+data = CSV.read(options[:file],
+                headers: true,
+                header_converters: [->(value) { value.strip rescue value }],
+                converters: [->(value) { value.strip rescue value }],
+                col_sep: "|")
+
+CmpToTestScript.new(data: data,
+                    output_file: options[:output_file],
+                    input_file: options[:file],
+                    verbose: options[:verbose]
+                   ).run
